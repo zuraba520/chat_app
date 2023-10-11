60fbd7dbb73ec674b57a9562a3a80095f3834448
gizu <zuraba000@gmail.com>
2023-10-12 03:38:18 +0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:chat_app/screens/auth.dart';
+
 void main() {
   runApp(const App());
 }
@@ -10,13 +12,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
-        ),
-        home: ...
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor:  Colors.orange),
+      ),
+      home: const AuthScreen(),
     );
   }
 }
